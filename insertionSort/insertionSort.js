678af179ee82e52cdd2e9710c8a7da0bb5fd7a9e
Phonekham <phone.madridista@gmail.com>
2019-11-22 08:53:19 +0700

    Initial commit

---
+++ insertionSort/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+    console.log("current val", currentVal);
+
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+      console.log("arr[j]", arr[j]);
+    }
+    arr[j + 1] = currentVal;
+    console.log(arr);
+    console.log("current val2", currentVal);
+  }
+  return arr;
+}
+insertionSort([2, 1, 9, 76, 4]);
